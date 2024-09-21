e32f1030d3ff2f6dfc16f8d21a61e4902b7d05d9
Alexia <161457494+alexbaxter07@users.noreply.github.com>
2024-09-21 14:11:44 +0100

    updated: boxes

--- ASDA.php
+++ ASDA.php
@@ -15,46 +15,55 @@
 
     <body>
 
-        <div id="content">
+        <div id="container">
 
+            <h3>Intro</h3>
             <div id="intro">
 
                 <p> During my week at ASDA house Leeds. I was learning about different departments within the groups of data analysts at ASDA. I had the opportunity to work with Girls in Data and see about there initiative to get more girls and young people into computer science/data. I also got the opportunity to shadow some people in legal and compliance and see the work that they do. I also got to shadow people in the home sector and also the customer feedback team. Along with all of that I got to experience using a new piece of software for me. I used PowerBi to create a dashboard showing the sales report for soft Drinks.</p>
-                <img src="3.png">
+
+                <div id="intro_img">
+                    <img src="3.png">
+                </div>
 
             </div>
 
-            <div id="bi">
+            <div id="content">
 
                 <h3>Power BI</h3>
-                <p>I used PowerBI to visualize sales reports of soft drinks in a user-friendly way so that they can see the top stores for sales and the top drinks. I used a slicer which means that a user can select a product to just see that products data. I also added backgrounds to the dashboard so that it is visually appealing to the end user. I found that the titles and headings for the dashboard need to be extremely clear or the data becomes hard to read or misleading. PowerBi is a very user-friendly piece of software and easy to use. It can be confusing at first however because you must link the databases, or the columns data will be incorrect and show false information.</p>
+                <div id="bi">
 
-                <div id="bi_img">
-                    <img src="BI.png">
+                    <p>I used PowerBI to visualize sales reports of soft drinks in a user-friendly way so that they can see the top stores for sales and the top drinks. I used a slicer which means that a user can select a product to just see that products data. I also added backgrounds to the dashboard so that it is visually appealing to the end user. I found that the titles and headings for the dashboard need to be extremely clear or the data becomes hard to read or misleading. PowerBi is a very user-friendly piece of software and easy to use. It can be confusing at first however because you must link the databases, or the columns data will be incorrect and show false information.</p>
+
+                    <div id="bi_img">
+                        <img src="BI.png">
+                    </div>
+
                 </div>
 
-            </div>
+                <h3>Girls in DATA/ Peoples Initiative</h3>
+                <div id="gid">
 
-            <div id="gid">
+                    <p>Girls in data is a program to get more girls interested in data and computer science in general. Whilst at ASDA I helped create some of the content and upgrade some of the content that was there that will be used in schools when ASDA go into schools to do the workshops. Girls in data is also trying to attract girls from a younger age to help them learn basic computer schools so that the younger generations know what a computer is and how to use them as it is a skillset that is no longer taught as more and more young people are getting more and more used to using laptops than computers so basic excel skills are getting missed when young people are learning it in mainly primary school. Some young people going into secondary schools now  have never even seen a standard desktop computer as these are no longer common in households now. It is more common to have tablets than laptops and desktop computers. This is very important to me as I would like to be able to get more girls into computer science as I have seen firsthand how male dominated the industry is and I would like to be able to encourage more young girls into this line of work as I believe it is not a very common subject for girls to be introduced into. Data is a very underappreciated side of computer science as well. So, I think the girls in data scheme is an amazing opportunity to get more girls into the subject and data. There are also many assumptions made about data that is misconstrued widely in schools that this tackles.</p>
 
-                <h3>Girls in DATA/ Peoples Initive</h3>
-                <p>Girls in data is a program to get more girls interested in data and computer science in general. Whilst at ASDA I helped create some of the content and upgrade some of the content that was there that will be used in schools when ASDA go into schools to do the workshops. Girls in data is also trying to attract girls from a younger age to help them learn basic computer schools so that the younger generations know what a computer is and how to use them as it is a skillset that is no longer taught as more and more young people are getting more and more used to using laptops than computers so basic excel skills are getting missed when young people are learning it in mainly primary school. Some young people going into secondary schools now  have never even seen a standard desktop computer as these are no longer common in households now. It is more common to have tablets than laptops and desktop computers. This is very important to me as I would like to be able to get more girls into computer science as I have seen firsthand how male dominated the industry is and I would like to be able to encourage more young girls into this line of work as I believe it is not a very common subject for girls to be introduced into. Data is a very underappreciated side of computer science as well. So, I think the girls in data scheme is an amazing opportunity to get more girls into the subject and data. There are also many assumptions made about data that is misconstrued widely in schools that this tackles.</p>
+                    <div id="gid_img">
+                        <img src="GID.png"
+                    </div>
 
-                <div id="gid_img">
-                    <img src="GID.png"
                 </div>
 
-            </div>
-
-            <div id="shadow">
                 <h3>Shadowing</h3>
-                <p>I shadowed people from the retail team who look at sales of products and show the different trends in sales of products and in store/ out of store shopping. They then feedback all that data into graphs to give to different higher ups to show where people are shopping more and how they are shopping and what products are most popular. This also feeds to the customer service team as they have very similar graphs which are based on customer feedback and to show which stores are getting the best/worst reviews. So, then people can see how well each store is doing and what they need to improve on and where they are losing customers to competitors. This is helpful for the business so that they can attract more customers and partners into the business. I also shadowed the legal and compliance team. They look at all of the different stores to see where they are failing and where they are doing well. This helps when it comes to the stores retaining customers as if the stores are clean and comply with all the rules and regulations it tends to bring more customers into the store. I also helped give feedback on their powerbi dashboards that they create to give to the different stores so they can clearly see what they need to improve on and where they do well so that they can do better for next time and so that the store can retain more customers and so that it also is allowed to stay open.</p>
+                <div id="shadow">
 
-                <div id="shadow_img">
-                    <img src="4.png">
+                    <p>I shadowed people from the retail team who look at sales of products and show the different trends in sales of products and in store/ out of store shopping. They then feedback all that data into graphs to give to different higher ups to show where people are shopping more and how they are shopping and what products are most popular. This also feeds to the customer service team as they have very similar graphs which are based on customer feedback and to show which stores are getting the best/worst reviews. So, then people can see how well each store is doing and what they need to improve on and where they are losing customers to competitors. This is helpful for the business so that they can attract more customers and partners into the business. I also shadowed the legal and compliance team. They look at all of the different stores to see where they are failing and where they are doing well. This helps when it comes to the stores retaining customers as if the stores are clean and comply with all the rules and regulations it tends to bring more customers into the store. I also helped give feedback on their powerbi dashboards that they create to give to the different stores so they can clearly see what they need to improve on and where they do well so that they can do better for next time and so that the store can retain more customers and so that it also is allowed to stay open.</p>
+
+                    <div id="shadow_img">
+                        <img src="4.png">
+                    </div>
+
                 </div>
 
-            </div>
+            </div> 
 
         </div>
 
